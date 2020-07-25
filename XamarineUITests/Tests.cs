204a2c18712b7d0ebc9488c8892e45037c8bfe71
Vitaly Zubkov <zubkov.v.o@gmail.com>
2020-07-25 14:28:07 +0300

    Started Automation project on Xamarine

---
+++ XamarineUITests/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace XamarineUITests
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+        public string _path = @"D:\Logs\test.apk";
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform, _path);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            app.Repl();
+        }
+    }
+}
